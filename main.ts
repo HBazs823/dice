f09db2d98ce2719a6ac9ec2d6edfa797d826aadb
HBazs823 <huszka.balazsl@gmail.com>
2025-11-12 08:44:01 +0100

    pxt.json, main.blocks, main.ts, pxt.json, _history frissítése

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
-input.onGesture(Gesture.Shake, function on_gesture_shake() {
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 4; index++) {
+        basic.showNumber(6)
+        basic.showNumber(7)
+    }
+})
+input.onGesture(Gesture.Shake, function () {
     basic.showNumber(randint(1, 5))
 })
